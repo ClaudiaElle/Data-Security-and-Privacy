29644cdf5c15836251dce8d6ae5b99c25225109d
Claudia Elle <102473341+ClaudiaElle@users.noreply.github.com>
2023-10-29 23:03:36 +0100

    Update AnalisiFrequenze.py
    Codice rivisto per consegna.

--- EserciziSDS/Parte1/AnalisiFrequenze.py
+++ EserciziSDS/Parte1/AnalisiFrequenze.py
@@ -10,7 +10,7 @@ def clean_text (text):
 
 def frequency_histogram(text):
     cleaned_text=clean_text(text)
-    counter = Counter(cleaned_text)
+    counter = Counter(cleaned_text)   # Crea un dizionario con le frequenze di ciascuna lettera
     letters, frequencies = zip(*sorted(counter.items()))   # Estrae le lettere e le relative occorrenze e ordina in ordine alfabetico    
     plt.bar(letters, frequencies)   # Creazione istogramma
     plt.xlabel('Letters')   # Aggiunge le etichette all'istogramma
@@ -20,28 +20,29 @@ def frequency_histogram(text):
 
 
 def mgram_distribution(text, m):
-    cleaned_text=clean_text(text) 
-    #mgram_distribution = Counter(cleaned_text[i:i + m] for i in range(len(cleaned_text) - m + 1)) #con sovrapposizione  
-    mgram_distribution = Counter(cleaned_text[i:i + m] for i in range(0, len(cleaned_text) - m + 1, m))
-    blocks, b_frequencies = zip(*sorted(mgram_distribution.items()))   # Estrae le lettere e le relative occorrenze e ordina in ordine alfabetico    
-    num_to_show = min(50, len(blocks))    
-    plt.bar(blocks[:num_to_show], b_frequencies[:num_to_show])
+    cleaned_text = clean_text(text)
+    mgram_distribution = Counter(cleaned_text[i:i + m] for i in range(0, len(cleaned_text) - m + 1, m))   # Crea un dizionario con le frequenze di ciascun m-gramma
+    total_mgrams = sum(mgram_distribution.values())
+    relative_frequencies = {mgram: freq / total_mgrams for mgram, freq in mgram_distribution.items()}  # Crea un dizionario con m-grammi come chiavi e frequenze relative come valori
+    blocks, _ = zip(*sorted(mgram_distribution.items()))  # Estrae gli m-grammi e li ordina in ordine alfabetico
+    num_to_show = min(50, len(blocks))   # Mostra solo le prime 50 frequenze
+    relative_frequencies_list = [relative_frequencies[mgram] for mgram in blocks[:num_to_show]]    # Estrae le frequenze relative in base alle chiavi dal dizionario
+    plt.bar(blocks[:num_to_show], relative_frequencies_list)  # Utilizza le frequenze relative
     plt.xlabel('Blocks')
-    plt.ylabel('Block Frequencies')
-    plt.title(f'{m}-Grams Frequency Histogram')
-    plt.xticks(rotation='vertical')
-    plt.show()   # Mostra l'istogramma  
+    plt.ylabel('Relative Frequencies')   # Modifica l'etichetta
+    plt.title(f'{m}-Grams Relative Frequency Histogram')   # Modifica il titolo
+    plt.xticks(rotation='vertical')   # Mette le etichette in verticale
+    plt.show()   # Mostra l'istogramma
     return mgram_distribution
 
 
-def coincidence_index(mgram_distribution):
+def coincidence_index(mgram_distribution):   # Calcola l'indice di coincidenza per blocchi di lunghezza m
     total_mgrams = sum(mgram_distribution.values())   # Calcola il numero totale di m-grammi
-    ic = sum((frequency*(frequency-1))for frequency in mgram_distribution.values())/(total_mgrams*(total_mgrams-1))     
-    # Calcola le frequenze relative, le eleva al quadrato e ne fa la somma
+    ic = sum((frequency*(frequency-1)) for frequency in mgram_distribution.values())/(total_mgrams*(total_mgrams-1))     
     return ic
 
 
-def shannon_entropy(mgram_distribution):
+def shannon_entropy(mgram_distribution):   # Calcola l'entropia di shannon per blocchi di lunghezza m
     total_mgrams = sum(mgram_distribution.values())
     entropy = -sum((frequency / total_mgrams) * math.log2(frequency / total_mgrams) for frequency in mgram_distribution.values())
     print(total_mgrams)
@@ -61,6 +62,3 @@ if __name__ == "__main__":
         entropy = shannon_entropy(distribution)   # Entropia della distribuzione degli m-grammi
         print(f"Indice di coincidenza con blocchi di lunghezza {i}: {ic}")
         print(f"Entropia di Shannon con blocchi di lunghezza {i}: {entropy}")
-
-
-
